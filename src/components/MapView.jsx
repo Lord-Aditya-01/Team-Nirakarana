b70ad0920df7b5f69cf29bb079a40769a25459f9
Lord-Aditya-01 <adityagirhe83@gmail.com>
2026-03-16 20:18:44 +0530

    changing manhole symbol

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,44 +1,58 @@
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import { useEffect, useState } from "react";
-import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-function MapView() {
+const MapView = () => {
 
   const [manholes, setManholes] = useState(null);
 
   useEffect(() => {
     fetch("/manholes.geojson")
-      .then(res => res.json())
-      .then(data => setManholes(data));
+      .then((res) => res.json())
+      .then((data) => setManholes(data))
+      .catch((err) => console.error("Error loading manholes:", err));
   }, []);
 
   return (
-    <MapContainer center={[17.67, 75.90]} zoom={12} style={{ height: "500px", width: "100%" }}>
+    <div style={{ height: "600px", width: "100%" }}>
       
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <MapContainer
+        center={[17.67, 75.90]} // Solapur center
+        zoom={12}
+        style={{ height: "100%", width: "100%" }}
+      >
 
-      {manholes && <GeoJSON
-      data={manholes}
-      pointToLayer={(feature, latlng) =>
-        L.circleMarker(latlng, {
-          radius: 4,
-          fillColor: "black",
-          color: "black",
-          weight: 1,
-          opacity: 1,
-          fillOpacity: 0.9,
-        })
-      }
-      onEachFeature={(feature, layer) => {
-        layer.bindPopup("Manhole ID: " + (feature.properties.id || "N/A"));
-      }}
-    />}
-      
-    </MapContainer>
+        <TileLayer
+          attribution="© OpenStreetMap contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {manholes && (
+          <GeoJSON
+            data={manholes}
+            pointToLayer={(feature, latlng) =>
+              L.circleMarker(latlng, {
+                radius: 4,
+                fillColor: "black",
+                color: "black",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.9,
+              })
+            }
+            onEachFeature={(feature, layer) => {
+              layer.bindPopup(
+                "Manhole ID: " + (feature.properties?.id || "N/A")
+              );
+            }}
+          />
+        )}
+
+      </MapContainer>
+
+    </div>
   );
-}
+};
 
 export default MapView;
