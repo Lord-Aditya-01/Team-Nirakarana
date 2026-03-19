798f5e3586dc769ec4134b6ec39e79336656d2cb
Parth-Shingane-27 <parthshingane27@gmail.com>
2026-03-19 16:34:06 +0530

    minor fix

--- safedrain-backend/config/db.js
+++ safedrain-backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 //mongodb+srv://nirakarana01_db_user:ZXufJjPJzJFh3loE@nirakarana.thj9qdh.mongodb.net/?appName=Nirakarana
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://nirakarana01_db_user:ZXufJjPJzJFh3loE@nirakarana.thj9qdh.mongodb.net/?appName=Nirakarana");
+    await mongoose.connect("mongodb+srv://nirakarana01_db_user:Samved@Nirakarana#01#@nirakarana.thj9qdh.mongodb.net/?appName=Nirakarana");
 
     console.log("MongoDB connected");
   } catch (error) {
